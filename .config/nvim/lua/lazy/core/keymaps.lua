b05180b422b59455d8a531efe46131cbd03f2d2a
kyooo8 <kyosuke.7876@gmail.com>
2025-11-06 00:45:15 +0900

    jjをescに

--- .config/nvim/lua/lazy/core/keymaps.lua
+++ .config/nvim/lua/lazy/core/keymaps.lua
@@ -2,6 +2,7 @@ vim.g.mapleader = ";"
 
 local keymap = vim.keymap
 
+keymap.set("i", "jj", "<Esc>", { noremap = true })
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })

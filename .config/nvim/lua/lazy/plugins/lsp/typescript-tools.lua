c2988cda665ec4b71ae5fdb29224e8ab571c65c4
kyooo8 <kyosuke.7876@gmail.com>
2026-07-18 22:59:53 +0900

    add ts-tools setting

--- .config/nvim/lua/lazy/plugins/lsp/typescript-tools.lua
+++ .config/nvim/lua/lazy/plugins/lsp/typescript-tools.lua
@@ -1,5 +1,12 @@
 return {
 	"pmizio/typescript-tools.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-	opts = {},
+	opts = {
+		on_attach = function(client)
+			local root = client.root_dir or vim.fn.getcwd()
+			if vim.fn.filereadable(root .. "/deno.json") == 1 or vim.fn.filereadable(root .. "/deno.jsonc") == 1 then
+				client.stop()
+			end
+		end,
+	},
 }

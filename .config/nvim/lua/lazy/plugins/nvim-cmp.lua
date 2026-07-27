a0df8391540b8d6b2035314a4329acd832c5d1b4
kyooo8 <kyosuke.7876@gmail.com>
2026-07-27 13:20:08 +0900

    lspkindの二重呼び出しを修正

--- .config/nvim/lua/lazy/plugins/nvim-cmp.lua
+++ .config/nvim/lua/lazy/plugins/nvim-cmp.lua
@@ -2,34 +2,29 @@ return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
 		{
 			"L3MON4D3/LuaSnip",
-			-- follow latest release.
-			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-			-- install jsregexp (optional!).
+			version = "v2.*",
 			build = "make install_jsregexp",
 		},
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets", -- useful snippets
-		"onsails/lspkind.nvim", -- vs-code like pictograms
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+		"onsails/lspkind.nvim",
 	},
 	config = function()
 		local cmp = require("cmp")
-
 		local luasnip = require("luasnip")
-
 		local lspkind = require("lspkind")
 
-		-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
 		require("luasnip.loaders.from_vscode").lazy_load()
 
 		cmp.setup({
 			completion = {
 				completeopt = "menu,menuone,preview,noselect",
 			},
-			snippet = { -- configure how nvim-cmp interacts with snippet engine
+			snippet = {
 				expand = function(args)
 					luasnip.lsp_expand(args.body)
 				end,
@@ -39,24 +34,51 @@ return {
 				["<C-k>"] = cmp.mapping.select_prev_item(),
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-				["<C-e>"] = cmp.mapping.abort(), -- close completion window
+				["<C-l>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
-			-- sources for autocompletion
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- snippets
-				{ name = "buffer" }, -- text within current buffer
-				{ name = "path" }, -- file system paths
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
 				{ name = "neorg" },
 			}),
 
-			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
 				format = lspkind.cmp_format({
 					maxwidth = 50,
 					ellipsis_char = "...",
+					preset = "codicons",
+					symbol_map = {
+						Text = "󰉿 ",
+						Method = "󰆧",
+						Function = "󰊕",
+						Constructor = " ",
+						Field = "󰜢",
+						Variable = " ",
+						Class = "󰠱",
+						Interface = " ",
+						Module = " ",
+						Property = "󰜢",
+						Unit = " ",
+						Value = "󰎠 ",
+						Enum = " ",
+						Keyword = " ",
+						Snippet = " ",
+						Color = "󰏘",
+						File = "󰈙 ",
+						Reference = "󰈇",
+						Folder = "󰉋",
+						EnumMember = " ",
+						Constant = "󰏿",
+						Struct = "󰙅",
+						Event = "",
+						Operator = "󰆕",
+						TypeParameter = " ",
+						Copilot = " ",
+					},
 				}),
 			},
 		})

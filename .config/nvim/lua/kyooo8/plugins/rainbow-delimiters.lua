33b93d1390385e5a17f8ef9df82f41587d83caa3
kyooo8 <kyosuke.aradono@kufu.co.jp>
2026-07-29 11:58:14 +0900

    赤色除外

--- .config/nvim/lua/kyooo8/plugins/rainbow-delimiters.lua
+++ .config/nvim/lua/kyooo8/plugins/rainbow-delimiters.lua
@@ -3,4 +3,16 @@ return {
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 	},
+	init = function()
+		vim.g.rainbow_delimiters = {
+			highlight = {
+				"RainbowDelimiterYellow",
+				"RainbowDelimiterBlue",
+				"RainbowDelimiterOrange",
+				"RainbowDelimiterGreen",
+				"RainbowDelimiterViolet",
+				"RainbowDelimiterCyan",
+			},
+		}
+	end,
 }

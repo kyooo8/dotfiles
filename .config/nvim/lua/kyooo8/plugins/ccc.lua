b640d23000c09b5deffbe1a577e32a7611f3ed77
kyooo8 <kyosuke.aradono@kufu.co.jp>
2026-08-20 10:48:14 +0900

    leader+c+hが動作するように変更

--- .config/nvim/lua/kyooo8/plugins/ccc.lua
+++ .config/nvim/lua/kyooo8/plugins/ccc.lua
@@ -1,10 +1,30 @@
 return {
 	"uga-rosa/ccc.nvim",
 	event = "VeryLazy",
+	keys = {
+		{ "<leader>cp", "<cmd>CccPick<CR>", desc = "Color picker" },
+		{
+			"<leader>ch",
+			function()
+				local bufnr = vim.api.nvim_get_current_buf()
+				local lsp_enabled = vim.lsp.document_color.is_enabled({ bufnr = bufnr })
+				local ccc_highlighter = require("ccc.highlighter")
+				local ccc_enabled = ccc_highlighter.attached_buffer[bufnr] == true
+				local enable = not (lsp_enabled or ccc_enabled)
+
+				vim.lsp.document_color.enable(enable, { bufnr = bufnr })
+
+				if enable then
+					ccc_highlighter:enable(bufnr)
+				else
+					ccc_highlighter:disable(bufnr)
+				end
+			end,
+			desc = "Toggle color highlight",
+		},
+	},
 	config = function()
 		require("ccc").setup()
-		vim.keymap.set("n", "<leader>cp", ":CccPick<CR>", { desc = "Color picker" })
 		vim.keymap.set("i", "<C-c>", "<Plug>(ccc-insert)", { desc = "Insert color code" })
-		vim.keymap.set("n", "<leader>ch", ":CccHighlighterToggle<CR>", { desc = "Toggle ccc highlight" })
 	end,
 }

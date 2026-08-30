a6600dec3dc710aa58804d4f4573c2a8b9403d58
kyooo8 <kyosuke.aradono@kufu.co.jp>
2026-08-30 20:29:23 +0900

    hyperの設定を追加

---
+++ .hyper.js
@@ -0,0 +1,36 @@
+"use strict";
+
+module.exports = {
+  config: {
+    fontSize: 12,
+    fontFamily: "JetBrainsMonoNL Nerd Font Mono, Cica",
+    padding: "4px 14px",
+    shellArgs: ["--login"],
+    disableLigatures: true,
+    preserveCWD: true,
+    theme: "catppuccin-macchiato",
+    paneNavigation: {
+      hotkeys: {
+        navigation: {
+          left: "cmd+h",
+          down: "cmd+j",
+          up: "cmd+k",
+          right: "cmd+l",
+        },
+      },
+    },
+    hyperBorder: {
+      borderColors: ["#ed8796", "#eed49f"],
+    },
+  },
+  plugins: [
+    "@catppuccin/hyper#1.0.8",
+    "hyperpower",
+    "gitrocket",
+    "hyper-tab-icons",
+    "hyper-active-tab",
+    "hyperborder",
+    "hyper-pane",
+  ],
+  keymaps: {},
+};

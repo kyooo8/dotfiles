8630ed658feabf0fb8de4006c400460fd47ae86f
kyooo8 <kyosuke.aradono@kufu.co.jp>
2026-09-03 19:45:02 +0900

    require('nvim-treesitter').setup() を使用

--- .config/nvim/lua/kyooo8/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/kyooo8/plugins/nvim-treesitter.lua
@@ -1,50 +1,55 @@
+local ensure_installed = {
+	"json",
+	"javascript",
+	"typescript",
+	"tsx",
+	"yaml",
+	"html",
+	"pug",
+	"slim",
+	"php",
+	"css",
+	"scss",
+	"vue",
+	"prisma",
+	"markdown",
+	"markdown_inline",
+	"svelte",
+	"graphql",
+	"bash",
+	"lua",
+	"vim",
+	"dockerfile",
+	"gitignore",
+	"query",
+	"vimdoc",
+	"c",
+}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
 	branch = "master",
-	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
+	build = function()
+		local treesitter = require("nvim-treesitter")
+		treesitter.install(ensure_installed):wait(300000)
+		treesitter.update(ensure_installed):wait(300000)
+	end,
+	lazy = false,
 	config = function()
 		local treesitter = require("nvim-treesitter")
 		treesitter.setup()
 		vim.treesitter.language.register("yaml", "eruby.yaml")
 
-		-- ensure these language parsers are installed
-		local ensure_installed = {
-			"json",
-			"javascript",
-			"typescript",
-			"tsx",
-			"yaml",
-			"html",
-			"pug",
-			"slim",
-			"php",
-			"css",
-			"scss",
-			"vue",
-			"prisma",
-			"markdown",
-			"markdown_inline",
-			"svelte",
-			"graphql",
-			"bash",
-			"lua",
-			"vim",
-			"dockerfile",
-			"gitignore",
-			"query",
-			"vimdoc",
-			"c",
-		}
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = ensure_installed,
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("kyooo8-treesitter", { clear = true }),
+			callback = function(args)
+				local has_parser = pcall(vim.treesitter.start, args.buf)
+				if not has_parser then
+					return
+				end
+
+				vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
 		})
 	end,
 }
